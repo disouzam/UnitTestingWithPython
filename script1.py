069cdb155bd93d7bdc46542623fde47de4c553b1
Dickson Souza <disouzam@yahoo.com.br>
2023-10-10 23:15:59 -0300

    Removed code duplication

--- script1.py
+++ script1.py
@@ -2,8 +2,9 @@ import pytest
 
 # Production code
 def fizzBuzz(value):
-    return
+    return "1"
 
 # Tests
-def test_canCallFizzBuz():
-    fizzBuzz(1)
+def test_return1With1PassedIn():
+    retVal = fizzBuzz(1)
+    assert retVal == "1"
