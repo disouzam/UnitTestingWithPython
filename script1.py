6ca634280a2f8cdc2448e6a1c7ff32c753fbb9c4
Dickson Souza <disouzam@yahoo.com.br>
2023-10-12 19:07:40 -0300

    Modified implementation and added test for input == 2

--- script1.py
+++ script1.py
@@ -2,9 +2,13 @@ import pytest
 
 # Production code
 def fizzBuzz(value):
-    return "1"
+    return str(value)
 
 # Tests
 def test_return1With1PassedIn():
     retVal = fizzBuzz(1)
     assert retVal == "1"
+
+def test_returns2With2Passedod():
+    retVal = fizzBuzz(2)
+    assert retVal == "2"
