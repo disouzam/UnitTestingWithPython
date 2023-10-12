2e08a7c537c7dd1c1a2ebcc405a7c29860f246e3
Dickson Souza <disouzam@yahoo.com.br>
2023-10-12 20:42:56 -0300

    Added another utility isMultiple and all necessary tests of FizzBuzz Kata

--- script1.py
+++ script1.py
@@ -2,7 +2,16 @@ import pytest
 
 # Production code
 def fizzBuzz(value):
+    if isMultiple(value, 3):
+        if isMultiple(value, 5):
+            return "FizzBuzz"
+        return "Fizz"
+    if isMultiple(value, 5):
+        return "Buzz"
     return str(value)
+
+def isMultiple(value, mod):
+    return (value % mod) == 0
 
 # Tests
 def checkFizzBuzz(value, expectRetVal):
@@ -14,3 +23,18 @@ def test_return1With1PassedIn():
 
 def test_returns2With2Passedod():
     checkFizzBuzz(2, "2")
+
+def test_returnsFizzWith3PassedIn():
+    checkFizzBuzz(3, "Fizz")
+
+def test_returnsBuzzWith5PassedIn():
+    checkFizzBuzz(5, "Buzz")
+
+def test_returnsFizzWith6PassedIn():
+    checkFizzBuzz(6, "Fizz")
+
+def test_returnBuzzWith10PassedIn():
+    checkFizzBuzz(10, "Buzz")
+
+def test_returnsFizzBuzzWith15PassedIn():
+    checkFizzBuzz(15, "FizzBuzz")
