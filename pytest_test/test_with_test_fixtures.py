55857aec50f723bbe3fa805443a00ab96b536622
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 13:56:31 -0300

    Test with Pytest fixtures
    - Fixture is not yet in use

---
+++ pytest_test/test_with_test_fixtures.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture()
+def setup():
+    print("\nSetup method")
+
+
+def test1():
+    print("\nExecuting test1!")
+    assert True
+
+
+def test2():
+    print("\nExecuting test2!")
+    assert True
