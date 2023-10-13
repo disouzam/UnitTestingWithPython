7451a7f4c2e4d9e0ce5921b356853bf650d12f7d
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 09:50:40 -0300

    Adhered to Python convention of adding self parameter in class methods

--- pytest_test/test_file.py
+++ pytest_test/test_file.py
@@ -1,13 +1,13 @@
 class TestClass:
-    def test_me():
+    def test_me(self):
         assert True
 
-    def test_me2():
+    def test_me2(self):
         assert True
 
 class MyTestClass():
-    def test_it():
+    def test_it(self):
         assert True
 
-    def test_it2():
+    def test_it2(self):
         assert True
