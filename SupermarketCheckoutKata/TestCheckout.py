0030248bc803cc761968fd0973343df2ac990c9f
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 16:58:11 -0300

    Introduced a fixture to initialize an instance of Checkout class

--- SupermarketCheckoutKata/TestCheckout.py
+++ SupermarketCheckoutKata/TestCheckout.py
@@ -1,11 +1,13 @@
+import pytest
 from Checkout import Checkout
 
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    return checkout
 
-def test_CanAddItemPrice():
-    co = Checkout()
-    co.addItemPrice("a", 1)
+def test_CanAddItemPrice(checkout):
+    checkout.addItemPrice("a", 1)
 
-
-def test_CanAddItem():
-    co = Checkout()
-    co.addItem("a")
+def test_CanAddItem(checkout):
+    checkout.addItem("a")
