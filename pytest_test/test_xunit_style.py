1bcec6ca2ac6c6f2b2087ee0e18fb9f23696cbfe
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 12:58:30 -0300

    Added inspect module to get function name dynamically

--- pytest_test/test_xunit_style.py
+++ pytest_test/test_xunit_style.py
@@ -1,3 +1,6 @@
+import inspect
+
+
 def setup_function(function):
     if function == test1:
         print("\nSetting up test1!")
@@ -6,6 +9,7 @@ def setup_function(function):
     else:
         print("\nSetting up test2!")
 
+
 def teardown_function(function):
     if function == test1:
         print("\nTearing down test1!")
@@ -13,6 +17,7 @@ def teardown_function(function):
         print("\nTearing down test2!")
     else:
         print("\nTearing down test2!")
+
 
 def test1():
     print("Executing " + (inspect.currentframe()).f_code.co_name + "!")
