20867acd012efb692d2a01acee3e715c04a93701
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 17:16:00 -0300

    Commnted out test methods removed by Richard Wells - training instructor - that I will reconsider later

--- SupermarketCheckoutKata/TestCheckout.py
+++ SupermarketCheckoutKata/TestCheckout.py
@@ -6,11 +6,14 @@ def checkout():
     checkout = Checkout()
     return checkout
 
-def test_CanAddItemPrice(checkout):
-    checkout.addItemPrice("a", 1)
-
-def test_CanAddItem(checkout):
-    checkout.addItem("a")
+# Tests removed by instructor Richard Wells since in his view they are not necessary anymore
+# though I prefer to have a slight duplication but to assert properly that those actions
+# are correctly performed
+# def test_CanAddItemPrice(checkout):
+#     checkout.addItemPrice("a", 1)
+#
+# def test_CanAddItem(checkout):
+#     checkout.addItem("a")
 
 def test_CanCalculateTotal(checkout):
     checkout.addItemPrice("a", 1)
