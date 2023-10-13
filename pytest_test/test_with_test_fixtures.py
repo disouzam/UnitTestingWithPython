a5a6999342bb032826e4a2e39ee58faa355d6c9a
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 14:26:37 -0300

    Use of autouse to activate setup fixtures

--- pytest_test/test_with_test_fixtures.py
+++ pytest_test/test_with_test_fixtures.py
@@ -1,17 +1,16 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def setup():
     print("\nSetup method")
 
 
-def test1(setup):
+def test1():
     print("\nExecuting test1!")
     assert True
 
 
-@pytest.mark.usefixtures("setup")
 def test2():
     print("\nExecuting test2!")
     assert True
