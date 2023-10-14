9c80877dc0369364422326004e237415e534973e
Dickson Souza <disouzam@yahoo.com.br>
2023-10-13 21:31:33 -0300

    Partial refactoring of tests and code

--- SupermarketCheckoutKata/TestCheckout.py
+++ SupermarketCheckoutKata/TestCheckout.py
@@ -32,3 +32,10 @@ def test_GetCorrectTotalWithMultipleItems(checkout):
 
 def test_canAddDiscountRule(checkout):
     checkout.addDiscount("a", 3, 2)
+
+def test_canApplyDiscountRule(checkout):
+    checkout.addItem("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 2
